47dd50cdd5ae10f1ec2332f7455a3d5e2fb4d7d1
Akash <akash.1822it1016@kiet.edu>
2022-04-29 08:31:26 +0530

    initial one

--- app.py
+++ app.py
@@ -19,42 +19,42 @@ def token_required(func):
             return jsonify({'Message': 'Invalid token'}), 403
         return func(*args, **kwargs)
     return decorated
-
-
 @app.route('/')
 def home():
-    if not session.get('logged_in'):
-        return render_template('login.html')
-    else:
-        return 'logged in currently'
+    login = False
+    if session["logged_in"]==True:
+        login=True
+    return render_template("index.html",login=login)
 
 @app.route('/register')
 def new_student():
-   return render_template('user.html')
+   return render_template('signup.html')
 
-@app.route('/adduser',methods = ['POST', 'GET'])
+@app.route('/adduser',methods = ['POST'])
 def addrec():
    if request.method == 'POST':
       try:
-         data=request.get_json()
-         print(data)
-         na=data["name"]
-         em=data["email"]
-         mo=data["mobile"]
-         pa=data["password"]
-        #  na = request.form['na']
-        #  em = request.form['em']
-        #  mo = request.form['mo']
-        #  pa = request.form['pa']
+        #  data=request.get_json()
+        #  print(data)
+        #  na=data["name"]
+        #  em=data["email"]
+        #  mo=data["mobile"]
+        #  pa=data["password"]
+         na = request.form['fullname']
+         em = request.form['email']
+         mo = request.form['number']
+         pa = request.form['password']
          with sql.connect("database.db") as con:
             cur = con.cursor()
-            
+            print("Hello")
             cur.execute("INSERT INTO  newuser(name,email,mobile,password) VALUES (?,?,?,?)",(na,em,mo,pa) )
             con.commit()
             msg = "Record successfully added"
+            print("added sucessfully")
       except:
          con.rollback()
          msg = "error in insert operation"
+         print("not added")
       
       finally:
          return redirect(url_for("home"))
@@ -78,40 +78,67 @@ def list():
 #    print(li)
    return make_response(jsonify(li))
 
-
-@app.route('/login', methods=['POST'])
+@app.route('/login')
 def login():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return 'logged in currently'
+
+@app.route('/loggedin', methods=['POST'])
+def loggedin():
     conn=sql.connect("database.db")
     curr=conn.cursor()
-    data=request.get_json()
-    print(data)
-    # uname=request.form['username']
-    # passw=request.form['password']
-    uname=data["username"]
-    passw=data["password"]
+    # data=request.get_json()
+    # print(data)
+    uname=request.form['username']
+    passw=request.form['password']
+    # uname=data["username"]
+    # passw=data["password"]
     print(uname)
     print(passw)
     curr.execute("SELECT * FROM newuser WHERE name=? and password=?",(uname,passw))
     rows=curr.fetchall()
     if len(rows)!=0:
         session['logged_in'] = True
+        session["user"]=uname
+        loggedin=True
         token = jwt.encode({
             'user': uname,
             # don't foget to wrap it in str function, otherwise it won't work [ i struggled with this one! ]
             'expiration': str(datetime.utcnow() + timedelta(seconds=60))
         },
             app.config['SECRET_KEY'])
-        return jsonify({'token': token.decode('utf-8')})
+        tokens = jsonify({'token': token.decode('utf-8')})
+        return redirect(url_for("home"))
     else:
-        return make_response('Unable to verify', 403, {'WWW-Authenticate': 'Basic realm: "Authentication Failed "'})
+        return redirect(url_for("login"))
 
 
 
 @app.route('/logout')
 def logout():
     session["logged_in"]=False
-    return "Logout"
+    session["user"]=""
+    loggedin=False
+    return redirect(url_for('home'))
 
+@app.route("/cart")
+def cart():
+    if not session.get('logged_in'):
+        return redirect(url_for("login"))
+    else:
+        return render_template("cart.html")
+
+
+@app.route('/auth')
+@token_required
+def auth():
+    return 'JWT is verified. Welcome to your dashboard !  '
+
+@app.route("/corona")
+def corona():
+    return render_template("corona.html")
 
 if __name__ == '__main__':
-   app.run(debug = True)
+   app.run(debug = False , host="localhost")
